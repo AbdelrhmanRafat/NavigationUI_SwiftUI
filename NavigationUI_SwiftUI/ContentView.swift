7dd15023bc5883fe6bf8840e9bf3354976bcf0e7
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-26 15:06:29 +0200

    Starter Project for Article Exercise.

--- NavigationUI_SwiftUI/ContentView.swift
+++ NavigationUI_SwiftUI/ContentView.swift
@@ -1,8 +1,8 @@
 //
 //  ContentView.swift
-//  SwiftUIList
+//  SwiftUITable
 //
-//  Created by Simon Ng on 9/9/2019.
+//  Created by Simon Ng on 6/9/2019.
 //  Copyright © 2019 AppCoda. All rights reserved.
 //
 
@@ -10,57 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var restaurants = [ Restaurant(name: "Cafe Deadend", image: "cafedeadend"),
-                        Restaurant(name: "Homei", image: "homei"),
-                        Restaurant(name: "Teakha", image: "teakha"),
-                        Restaurant(name: "Cafe Loisl", image: "cafeloisl"),
-                        Restaurant(name: "Petite Oyster", image: "petiteoyster"),
-                        Restaurant(name: "For Kee Restaurant", image: "forkeerestaurant"),
-                        Restaurant(name: "Po's Atelier", image: "posatelier"),
-                        Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery"),
-                        Restaurant(name: "Haigh's Chocolate", image: "haighschocolate"),
-                        Restaurant(name: "Palomino Espresso", image: "palominoespresso"),
-                        Restaurant(name: "Homei", image: "upstate"),
-                        Restaurant(name: "Traif", image: "traif"),
-                        Restaurant(name: "Graham Avenue Meats And Deli", image: "grahamavenuemeats"),
-                        Restaurant(name: "Waffle & Wolf", image: "wafflewolf"),
-                        Restaurant(name: "Five Leaves", image: "fiveleaves"),
-                        Restaurant(name: "Cafe Lore", image: "cafelore"),
-                        Restaurant(name: "Confessional", image: "confessional"),
-                        Restaurant(name: "Barrafina", image: "barrafina"),
-                        Restaurant(name: "Donostia", image: "donostia"),
-                        Restaurant(name: "Royal Oak", image: "royaloak"),
-                        Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")
-    ]
-    init() {
-        let navBarAppearance = UINavigationBarAppearance() // NavigationBarAppearance API Customize Navigation Bar View.
-        navBarAppearance.largeTitleTextAttributes = [.foregroundColor : UIColor.systemRed, .font : UIFont(name: "ArialRoundedMTBold", size: 35)!]
-        navBarAppearance.titleTextAttributes = [.foregroundColor : UIColor.systemBlue, .font : UIFont(name: "ArialRoundedMTBold", size: 20)!]
-        navBarAppearance.setBackIndicatorImage(UIImage(systemName: "arrow.turn.up.left"), transitionMaskImage: UIImage(systemName: "arrow.turn.up.left")) // Change Back Button Image.
-        UINavigationBar.appearance().tintColor = .black
-        // Change Back Button Color.
-        UINavigationBar.appearance().standardAppearance = navBarAppearance // Appearance of standard-Height Navigation Bar.
-        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance // Appearance when the edge of the scrolled content reaches the navigation Bar.
-        UINavigationBar.appearance().compactAppearance = navBarAppearance //Appearance of compact height Navigation Bar.
-    }
-    
     var body: some View {
-        NavigationView
-        {
-            List {
-                ForEach(restaurants) { restaurant in
-                    //Navigation Link is a Special button detect User's touch and triggers the Navigation Presentation.
-                    NavigationLink(
-                        destination: RestautantDetailView(restaurant: restaurant), // Specify Destination view in Destination Paramater.
-                        label: {
-                            BasicImageRow(restaurant: restaurant) // Implement Cell view in the closure
-                        })
-                }
-            }
-            .navigationBarTitle("Restaurants",displayMode: NavigationBarItem.TitleDisplayMode.automatic)
-            // Display Mode change view of navigation Bar.
-            
+        
+        List(articles) { article in
+                
+            ArticleRow(article: article)
+                
         }
+ 
     }
 }
 
@@ -70,40 +27,41 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Restaurant: Identifiable {
-    var id = UUID()
-    var name: String
-    var image: String
-}
 
-struct BasicImageRow: View {
-    var restaurant: Restaurant
+struct ArticleRow: View {
+    var article: Article
     
     var body: some View {
-        HStack {
-            Image(restaurant.image)
+        VStack(alignment: .leading) {
+            Image(article.image)
                 .resizable()
-                .frame(width: 40, height: 40)
+                .aspectRatio(contentMode: .fit)
                 .cornerRadius(5)
-            Text(restaurant.name)
+            
+            Text(article.title)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+                .lineLimit(3)
+                .padding(.bottom, 0)
+            
+            Text("By \(article.author)".uppercased())
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .padding(.bottom, 0)
+            
+            HStack(spacing: 3) {
+                ForEach(1...(article.rating), id: \.self) { _ in
+                    Image(systemName: "star.fill")
+                        .font(.caption)
+                        .foregroundColor(.yellow)
+                }
+            }
+            
+            Text(article.excerpt)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
         }
     }
 }
 
-struct RestautantDetailView : View {
-    var restaurant : Restaurant
-    var body: some View {
-        VStack {
-            Image(restaurant.image)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: .infinity, height: 300, alignment: .center)
-                .clipped()
-                .padding(.horizontal)
-            Text(restaurant.name)
-                .font(.system(.title, design: .rounded))
-                .fontWeight(.black)
-            Spacer()
-        }
-    }
-}
